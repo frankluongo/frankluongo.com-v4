5113784163a58a721932da327599bf9f255c8f36
frankluongo <frank@frankluongo.com>
2021-02-12 08:17:15 -0500

    Format article

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,12 +3,13 @@ import { Link } from "gatsby"
 
 import { Button } from "@components/Common"
 
+// import "./GitHubMarkdown.css"
 import styles from "./Post.module.css"
 
 export default function Post({ children }) {
   return (
     <>
-      <section className={styles.Post}>{children}</section>
+      <section className={`${styles.Post} markdown-body`}>{children}</section>
       <div className={styles.CallToAction}>
         <Button Tag={Link} to="/blog" data-theme="dark">
           Back to Blog
